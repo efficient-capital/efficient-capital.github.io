ea699c566378d9d99665459fc72e7fa0ebbb3fd7
Matas Pocevicius <matas.pocevicius@gmail.com>
2020-02-07 18:35:14 +0100

    enviroment setup, julius bar started

---
+++ precache-manifest.fb977013501476a9ecda96d18a10c85b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85e67b520fcd41c4aedc6ab3966ec05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89bcca66456163f3970c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ed8aae2ecb40c8fe0be1",
+    "url": "/static/js/2.f66d9fcc.chunk.js"
+  },
+  {
+    "revision": "89bcca66456163f3970c",
+    "url": "/static/js/main.7384f38b.chunk.js"
+  },
+  {
+    "revision": "4504959292c5f516808d",
+    "url": "/static/js/runtime~main.c23a52a6.js"
+  }
+]);
